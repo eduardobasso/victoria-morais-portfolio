46d25d82fa15d7a0676f98dfea52fe09f6c7e0dd
Eduardo Basso <ebmoraesjr@gmail.com>
2022-03-10 19:16:45 -0300

    First commit on dev branch

--- components/work-section.tsx
+++ components/work-section.tsx
@@ -16,7 +16,7 @@ function WorkSection() {
             imgSrc="/images/projects/brastemp-experience/thumb.png"
             imgAlt="Brastemp cover"
             imgPos="right"
-            route="/"
+            route="/work/brastemp-experience"
           />
           <ProjectCard
             company="Validation concept"
