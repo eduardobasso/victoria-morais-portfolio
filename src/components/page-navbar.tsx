e6a7238ab3d2ecea504a47c156032a45a30c55a1
Eduardo Basso <ebmoraesjr@gmail.com>
2024-12-17 18:24:58 +0000

    Update resume

--- src/components/page-navbar.tsx
+++ src/components/page-navbar.tsx
@@ -52,7 +52,7 @@ function PageNavbar() {
                                     bgcolor: alpha(theme.palette.common.white, 0.3),
                                 },
                             })}
-                            href="/documents/resume-victoria-morais-santos-20240905.pdf"
+                            href="/documents/resume-victoria-morais-santos-20241217.pdf"
                             target="_blank"
                         >
                             {`Resume`}
